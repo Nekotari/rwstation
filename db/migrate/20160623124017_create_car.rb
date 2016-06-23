19a4528b821b7c83439efba4959d31bf14c42e19
Runae <stella_nemesis@live.com>
2016-06-23 17:10:05 +0400

    association

---
+++ db/migrate/20160623124017_create_car.rb
@@ -0,0 +1,12 @@
+class CreateCar < ActiveRecord::Migration
+  def change
+    create_table :cars do |t|
+      t.integer :number
+      t.string :type
+      t.integer :upper
+      t.integer :lower
+      t.integer :train_id
+      t.timestamps null: false
+    end
+  end
+end
