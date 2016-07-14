f8ac58cd07e9e1b3d4b0187789c22a2f0795a5d2
Runae <stella_nemesis@live.com>
2016-07-14 14:51:03 +0400

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623124017) do
+ActiveRecord::Schema.define(version: 20160714103650) do
 
   create_table "cars", force: :cascade do |t|
     t.integer  "number"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160623124017) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "num"
   end
 
   create_table "routes", force: :cascade do |t|
