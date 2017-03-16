2c87352a5c3cfadbe4d0946de874ba1de4c24638
Runae <stella_nemesis@live.com>
2017-03-16 09:58:54 +0300

    Added mailer

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,21 @@ class Ticket < ActiveRecord::Base
   belongs_to :train
   belongs_to :from, class_name: 'RailwayStation', foreign_key: :from_id
   belongs_to :to, class_name: 'RailwayStation', foreign_key: :to_id
+
+  after_create :send_buy_notification
+  after_destroy :send_delete_notification
+
+  def route_name
+    "#{from.title} - #{to.title}"
+  end
+
+  private
+
+  def send_buy_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
+
+  def send_delete_notification
+    TicketsMailer.delete_ticket(self.user, self).deliver_now
+  end
 end
